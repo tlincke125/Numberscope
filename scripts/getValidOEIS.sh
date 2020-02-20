d9f4d772bbc6206fe83b898800c7f66cd666afb1
tlincke125 <thli2739@colorado.edu>
2020-02-19 17:44:36 -0700

    initial

--- scripts/getValidOEIS.sh
+++
@@ -1,8 +0,0 @@
-
-
-if [ ! -f website/javascript/core/validOEIS.js ]; 
-then 
-    curl https://raw.githubusercontent.com/sagemath/sagelib/master/sage/combinat/sloane_functions.py |
-    grep -Po "^class \K(A\d+)" |
-    python3 -c "import sys,json; print('export const validOEIS = ' + json.dumps(sys.stdin.read().split('\n')[:-1]))" > "../website/javascript/validOEIS.js"
-fi
